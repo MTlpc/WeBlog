518741157a69f3d98ca90909f42c56ede743375a
root <928062670@qq.com>
2020-07-24 22:39:37 +0800

    security enhancement

--- weblog/app.py
+++ weblog/app.py
@@ -8,6 +8,11 @@ from .handlers import blueprint_list
 from .configs import configs
 # 数据库Model类
 from .models import db, Role, User
+# 数据库迁移类
+from flask_migrate import Migrate 
+# 解决MySQL_DB不存在的问题
+import pymysql
+pymysql.install_as_MySQLdb()
 
 # 注册蓝图
 def register_blueprints(app):
@@ -23,6 +28,8 @@ def register_extensions(app):
     db.init_app(app)
     # 时间日期本地化
     Moment(app)
+    # 数据库迁移
+    Migrate(app, db) 
 
 
 # 创建flask入口
--- weblog/models.py
+++ weblog/models.py
@@ -1,4 +1,6 @@
 from flask_sqlalchemy import SQLAlchemy
+# 导入密码散列包
+from werkzeug.security import generate_password_hash, check_password_hash
 
 
 db = SQLAlchemy()
@@ -17,8 +19,22 @@ class Role(db.Model):
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(64), unique=True, index=True)
+    email = db.Column(db.String(64), unique=True, index=True)
+    _password = db.Column('password', db.String(128))
     role_id = db.Column(db.Integer, db.ForeignKey('role.id'))
     role = db.relationship('Role', backref=db.backref('users', lazy='dynamic'))
+    
+    @property
+    def password(self):
+        return self._password
+
+    @password.setter
+    def password(self, pwd):
+        self._password = generate_password_hash(pwd)
+
+    # 验证用户密码
+    def verify_password(self, pwd):
+        return check_password_hash(self._password, pwd)
 
     def __repr__(self):
         return '<User: {}>'.format(self.name)
